1e4aa936c2b4634c90258de7feb040950fa05b35
ramadis <rramiro.o@hotmail.com>
2018-09-17 18:16:32 +0200

    /meals return meals of every user

--- Goal.md
+++ Goal.md
@@ -1,13 +1,8 @@
-# Frontend
-* Filter by dates from-to, time from-to (e.g. how much calories have I had for lunch each day in the last month if lunch is between 12 and 15h).
-
 # Backend
 
 * Additionally, provide an option for the user to log in using at least two social media providers (you can pick from Google, Facebook, Twitter, Github, or similar).
 
 * If users log in using a social media pull the image from their account they used to log in.
-
-* Return in /meals ALL the meals
 
 * Write unit and e2e tests.
 
--- routes/api/meals.js
+++ routes/api/meals.js
@@ -24,65 +24,6 @@ router.param("meal", function(req, res, next, id) {
 });
 
 router.get("/", auth.required, hasRoles(["admin"]), withDateFilters, function(req, res, next) {
-  var query = {};
-  var limit = 20;
-  var offset = 0;
-
-  if (typeof req.query.limit !== "undefined") {
-    limit = req.query.limit;
-  }
-
-  if (typeof req.query.offset !== "undefined") {
-    offset = req.query.offset;
-  }
-
-  if (typeof req.query.tag !== "undefined") {
-    query.tagList = { $in: [req.query.tag] };
-  }
-
-  Promise.all([
-    req.query.author ? User.findOne({ username: req.query.author }) : null,
-    req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
-  ])
-    .then(function(results) {
-      var author = results[0];
-      var favoriter = results[1];
-
-      if (author) {
-        query.author = author._id;
-      }
-
-      if (favoriter) {
-        query._id = { $in: favoriter.favorites };
-      } else if (req.query.favorited) {
-        query._id = { $in: [] };
-      }
-
-      return Promise.all([
-        Meal.find(query)
-          .limit(Number(limit))
-          .skip(Number(offset))
-          .sort({ createdAt: "desc" })
-          .populate("author")
-          .exec(),
-        Meal.count(query).exec(),
-        req.payload ? User.findById(req.payload.id) : null
-      ]).then(function(results) {
-        var meals = results[0];
-        var mealsCount = results[1];
-        var user = results[2];
-
-        return res.json({
-          meals: meals.map(function(meal) {
-            return meal.toJSON();
-          }),
-          mealsCount: mealsCount
-        });
-      });
-    })
-    .catch(next);
-});
-router.get("/feed", auth.required, withDateFilters, function(req, res, next) {
   var limit = 20;
   var offset = 0;
 
@@ -106,6 +47,45 @@ router.get("/feed", auth.required, withDateFilters, function(req, res, next) {
         .populate("author")
         .exec(),
       Meal.count(req.filters.datetime)
+    ])
+      .then(function(results) {
+        var meals = results[0];
+        var mealsCount = results[1];
+
+        return res.json({
+          meals: meals.map(function(meal) {
+            return meal.toJSON();
+          }),
+          mealsCount: mealsCount
+        });
+      })
+      .catch(next);
+  });
+});
+router.get("/feed", auth.required, withDateFilters, function(req, res, next) {
+  var limit = 20;
+  var offset = 0;
+
+  if (typeof req.query.limit !== "undefined") {
+    limit = req.query.limit;
+  }
+
+  if (typeof req.query.offset !== "undefined") {
+    offset = req.query.offset;
+  }
+
+  User.findById(req.payload.id).then(function(user) {
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    Promise.all([
+      Meal.find(Object.assign({}, req.filters.datetime, { author: user._id }))
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .populate("author")
+        .exec(),
+      Meal.count(Object.assign({}, req.filters.datetime, { author: user._id }))
     ])
       .then(function(results) {
         var meals = results[0];
